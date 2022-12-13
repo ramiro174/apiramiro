fb4cca465c963b24d3e5869cf360d344b5920cbc
Ramiro <ramiro174@gmail.com>
2022-12-12 23:16:22 -0600

    agregue cambios de api

--- routes/api.php
+++ routes/api.php
@@ -24,16 +24,27 @@
 	});
 	Route::get('/ganadores', function (Request $request) {
 		$respuesta = ["status" => true];
-		$array =  Persona::all();
+		$array = Persona::all();
 		$respuesta["data"] = $array;
 		return Response::json($respuesta);
 	});
 	Route::post("/enviarnumero", function (Request $request) {
+//		["nombre" => $nombre, "numero" => $numero] ;
 		$persona = $request->input("persona");
+		if ($persona["numero"] >= 22) {
+			$respuesta = ["status" => false, "mensaje" => "El numero debe ser menor a 22"];
+			return Response::json($respuesta);
+		}
 		Persona::create($persona);
 		return Response::json($persona);
 	});
 	Route::post("/limpiar", function (Request $request) {
 		persona::truncate();
-		return Response::json(["status" => true,"data"=>"jajajajaaj"]);
+		return Response::json(["status" => true, "data" => "jajajajaaj"]);
+	});
+	
+	
+	Route::get('/contactos', function (Request $request) {
+		$respuesta["data"] =[["nombre" => "Juan", "numero" => "871-176-50-65","url"=>"https://www.google.com"], ["nombre" => "Pedro", "numero" => "871-176-50-64","url"=>"https://www.google.com"], ["nombre" => "Maria", "numero" => "871-176-50-62","url"=>"https://www.google.com"]];
+		return Response::json($respuesta);
 	});
